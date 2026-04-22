bff6a74a12cbd76b99462ebf8cf89aca7e9358f2
tamcv <chauviettam@gmail.com>
2026-04-22 11:56:59 +0700

    chore: release v0.5.0

--- Sources/Version.swift
+++ Sources/Version.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum AppVersion {
-    static let current = "0.4.3"
+    static let current = "0.5.0"
 
     // Build timestamp - updated during compilation
     static let buildDate: String = {
